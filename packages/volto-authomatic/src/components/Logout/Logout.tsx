f5536c541957cc37b5c62165f624b10ff0bac17b
Alok Kumar <ialokkumarsingh0@gmail.com>
2025-09-25 19:36:22 +0530

    Add support for redirecting users after logout. (#43)
    * Add support for redirecting users after logout.
    
    * add the default value
    
    * minor fix
    
    * improve code

--- packages/volto-authomatic/src/components/Logout/Logout.tsx
+++ packages/volto-authomatic/src/components/Logout/Logout.tsx
@@ -3,13 +3,19 @@
  * @module components/Logout/Logout
  */
 
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useMemo } from 'react';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import qs from 'query-string';
+import Toast from '@plone/volto/components/manage/Toast/Toast';
+import { toast } from 'react-toastify';
+import { defineMessages, useIntl } from 'react-intl';
 
 import { logout } from '@plone/volto/actions/userSession/userSession';
 import { purgeMessages } from '@plone/volto/actions/messages/messages';
 import { oidcLogout } from '@plone-collective/volto-authomatic/actions/auth/oidc';
 import { listActions } from '@plone/volto/actions/actions/actions';
+import config from '@plone/volto/registry';
 
 import LogoutPage from './LogoutPage';
 
@@ -22,39 +28,76 @@ interface RootState {
   };
 }
 
-const Logout: React.FC = () => {
-  const dispatch = useDispatch();
-  const [displayLogout, setDisplayLogout] = useState(false);
-  const logoutSuccess = useSelector((state: any) => state.userSession.token);
+const messages = defineMessages({
+  loggedOut: {
+    id: 'Logged out',
+    defaultMessage: 'Logged out',
+  },
+  loggedOutContent: {
+    id: 'You have been logged out from the site.',
+    defaultMessage: 'You have been logged out from the site.',
+  },
+});
 
+const Logout: React.FC = () => {
+  const displayLogout = config.settings.displayLogout;
+  const token = useSelector((state) => state.userSession.token, shallowEqual);
+  const history = useHistory();
+  const location = useLocation();
+  const intl = useIntl();
+
+  const dispatch = useDispatch();
   const OIDCValues = useSelector((state: RootState) => state.oidcRedirect);
   const logoutOIDCValues = useSelector((state: RootState) => state.oidcLogout);
+  const next_url = logoutOIDCValues.next_url;
+
+  const returnUrl = useMemo(
+    () =>
+      qs.parse(location.search).return_url ||
+      location.pathname
+        .replace(/\/login\/?$/, '')
+        .replace(/\/logout\/?$/, '') ||
+      '/',
+    [location.pathname, location.search],
+  );
+  useEffect(() => {
+    if (!token && !displayLogout && !next_url) {
+      history.replace(returnUrl || '/');
+      if (!toast.isActive('loggedOut')) {
+        toast.info(
+          <Toast
+            info
+            title={intl.formatMessage(messages.loggedOut)}
+            content={intl.formatMessage(messages.loggedOutContent)}
+          />,
+          { autoClose: false, toastId: 'loggedOut' },
+        );
+      }
+    }
+  }, [history, returnUrl, intl, token, next_url, displayLogout]);
 
   useEffect(() => {
     const isOIDC = OIDCValues.next_url;
     if (isOIDC) {
       dispatch(oidcLogout());
-    } else {
-      setDisplayLogout(true);
     }
     dispatch(logout());
     dispatch(purgeMessages());
   }, [dispatch, OIDCValues]);
 
   useEffect(() => {
-    if (displayLogout && logoutSuccess === null) {
+    if (displayLogout && token === null) {
       dispatch(listActions('/'));
     }
-  }, [displayLogout, dispatch, logoutSuccess]);
+  }, [displayLogout, dispatch, token]);
 
   useEffect(() => {
-    const next_url = logoutOIDCValues.next_url;
     if (next_url) {
       setTimeout(() => {
         window.location.href = next_url;
       }, 500);
     }
-  }, [logoutOIDCValues]);
+  }, [logoutOIDCValues, next_url]);
 
   return displayLogout ? <LogoutPage /> : null;
 };
