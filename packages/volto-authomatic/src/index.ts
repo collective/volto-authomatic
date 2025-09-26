384e5bd2f3dfc0987eaf0dbe299a428fb8d7c661
Alok Kumar <ialokkumarsingh0@gmail.com>
2025-09-26 17:41:41 +0530

    import settings config file to index folder. (#44)
    * import settings config file to index folder.
    
    * minor fix

--- packages/volto-authomatic/src/index.ts
+++ packages/volto-authomatic/src/index.ts
@@ -2,11 +2,13 @@ import type { ConfigType } from '@plone/registry';
 
 import installReducers from './config/reducers';
 import installRoutes from './config/routes';
+import installSettings from './config/settings';
 import './styles.css';
 
 export function applyConfig(config: ConfigType) {
   installReducers(config);
   installRoutes(config);
+  installSettings(config);
   return config;
 }
 
